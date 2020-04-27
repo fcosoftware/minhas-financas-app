473a8f92af4b1c5e3d3bdc2e5c753e56b2d45570
fcosoftware <fcosoftware35@gmail.com>
2020-04-27 17:11:29 -0300

    front end test 1

---
+++ src/views/lancamentos/cadastro-lancamentos.js
@@ -0,0 +1,202 @@
+import React from 'react'
+
+import Card from '../../components/card'
+import FormGroup from '../../components/form-group'
+import SelectMenu from '../../components/selectMenu'
+
+import { withRouter } from 'react-router-dom'
+import * as messages from '../../components/toastr'
+
+import Lancamentosservice from '../../app/service/lancamentoService'
+import LocalStorageService from '../../app/service/localStorageService'
+
+class CadastroLancamentos extends React.Component {
+
+    state = {
+        id: null,
+        descricao: '',
+        valor: '',
+        mes: '',
+        ano: '',
+        tipo: '', 
+        status: '',
+        usuario: null,
+        atualizando: false
+    }
+
+    constructor(){
+        super();
+        this.service = new Lancamentosservice();
+    }
+
+    componentDidMount(){
+        const params = this.props.match.params
+        
+            if(params.id){
+                this.service
+            .obterPorId(params.id)
+            .then( response => {
+                this.setState( {...response.data, atualizando: true} )
+            
+            }).catch( erros => {
+                messages.mensagemErro(erros.response.data)
+            })
+        }
+    }
+
+    salvarLancamento = () => {
+
+        const usuarioLogado = LocalStorageService.obterItem('_usuario_logado')
+
+        const { descricao, valor, mes, ano, tipo } = this.state;
+
+        const lancamento = { descricao, valor, mes, ano, tipo, usuario: usuarioLogado.id };
+
+        try {
+            this.service.validar(lancamento)
+        } catch (erro) {
+            const mensagens = erro.mensagens;
+            mensagens.forEach(msg => messages.mensagemErro(msg));
+            return false; 
+        }
+       
+        this.service
+            .salvar(lancamento)
+            .then( response => {
+                this.props.history.push('/consulta-lancamentos')
+                messages.mensagemSucesso('Lançamento cadastrado com sucesso!')
+            }).catch( error => {
+                messages.mensagemErro(error.response.data)
+            })
+
+    }
+
+    atualizar = () => {
+        const { descricao, valor, mes, ano, tipo, status, usuario, id } = this.state;
+
+        const lancamento = { descricao, valor, mes, ano, tipo, status, usuario, id };
+
+        this.service
+            .atualizar(lancamento)  
+            .then( response => {
+                this.props.history.push('/consulta-lancamentos')
+                messages.mensagemSucesso('Lançamento atualizado com sucesso!')
+            }).catch( error => {
+                messages.mensagemErro(error.response.data)
+            })  
+    }
+
+    handleChange = (event) => {
+        const value = event.target.value;
+        const name = event.target.name;
+
+        this.setState({ [name] : value })
+
+    }
+
+    render(){
+
+        const tipos = this.service.obterListaTipos();
+        const meses = this.service.obterListaMeses();
+
+        return(
+            <Card title={ this.state.atualizando ? 'Atualização de Lançamento' : 'Cadastro de Lançamento' }>
+                <div className="row">
+                    <div className="col-md-12"> 
+                        <FormGroup htmlFor="inputDescricao" label="Descrição: *">
+                            <input id="inputDescricao" 
+                                   type="text" 
+                                   className="form-control" 
+                                   name="descricao"
+                                   value={this.state.descricao}
+                                   onChange={this.handleChange}/>
+                        </FormGroup>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <FormGroup htmlFor="inputAno" label="Ano: *" >
+                            <input id="inputAno" 
+                                   type="text" 
+                                   name="ano"
+                                   value={this.state.ano}
+                                   onChange={this.handleChange}
+                                   className="form-control" />
+                        </FormGroup>
+                    </div>
+
+                    <div className="col-md-6">
+                        <FormGroup htmlFor="inputMes" label="Mês: *" >
+                        <SelectMenu id="inputMes" 
+                                    name="mes"
+                                    value={this.state.mes}
+                                    onChange={this.handleChange}
+                                    lista={meses} 
+                                    className="form-control"/>
+                        </FormGroup>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <FormGroup htmlFor="inputValor" label="Valor: *" >
+                            <input id="inputValor" 
+                                   type="text" 
+                                name="valor"
+                                value={this.state.valor}
+                                onChange={this.handleChange}
+                                className="form-control" />
+                        </FormGroup>
+                    </div>
+                    <div className="col-md-4">
+                        <FormGroup htmlFor="inputTipo" label="Tipo: *" >
+                            <SelectMenu id="inputTipo" 
+                                        name="tipo"
+                                        value={this.state.tipo}
+                                        onChange={this.handleChange}
+                                        lista={tipos} 
+                                        className="form-control"/>
+                        </FormGroup>
+                    </div>
+                    <div className="col-md-4">
+                        <FormGroup htmlFor="inputStatus" label="Status: " >
+                            <input type="text" 
+                                   name="status"
+                                   value={this.state.status}
+                                   onChange={this.handleChange}
+                                   className="form-control" 
+                                   disabled={true} />
+                        </FormGroup>
+                    </div>    
+                    
+                </div>
+                <div className="row">
+                        <div className="col-md-6">
+                            {   this.state.atualizando ? 
+                                (
+                                    <button 
+                                        onClick={this.atualizar} 
+                                        className="btn btn-primary">
+                                        <i className="pi pi-refresh"></i> Atualizar
+                                    </button>
+                                ) : (
+                                    <button 
+                                        onClick={this.salvarLancamento} 
+                                        className="btn btn-success">
+                                        <i className="pi pi-save"></i> Salvar
+                                    </button>
+                                )
+                            }
+                                    <button 
+                                        onClick={e => this.props.history
+                                        .push('/consulta-lancamentos')} 
+                                        className="btn btn-danger">
+                                        <i className="pi pi-arrow-circle-left"></i> Cancelar
+                                    </button>
+                        </div>
+                    </div>
+            </Card>
+        )
+    }
+}
+
+export default withRouter(CadastroLancamentos);
